c4988b95459250bcc39a25459ac036d60ed1fc5f
Tomer <tomerguet@gmail.com>
2020-10-08 19:45:41 +0300

    Updates

---
+++ precache-manifest.a5c3bc840a3c6c72c055ab4a0ca00e8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ddb50ed6a45a9bf26c18e003d401a5",
+    "url": "/Tomer-guetta/Robots/index.html"
+  },
+  {
+    "revision": "637c218122bea8cc63b6",
+    "url": "/Tomer-guetta/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0c7e26ff0201e7be26b0",
+    "url": "/Tomer-guetta/Robots/static/css/main.c293ca95.chunk.css"
+  },
+  {
+    "revision": "637c218122bea8cc63b6",
+    "url": "/Tomer-guetta/Robots/static/js/2.9bf23b2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tomer-guetta/Robots/static/js/2.9bf23b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7e26ff0201e7be26b0",
+    "url": "/Tomer-guetta/Robots/static/js/main.4291013a.chunk.js"
+  },
+  {
+    "revision": "1c4624375e104de80ffe",
+    "url": "/Tomer-guetta/Robots/static/js/runtime-main.36a5d0c0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Tomer-guetta/Robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
